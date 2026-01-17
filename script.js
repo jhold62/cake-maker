9448e3656f5702a6862c77a8fee0e46a6df49e56
jhold62 <mygov8527@gmail.com>
2026-01-17 23:25:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const cake = document.getElementById('cake');
 
+function addTopping(type) {
+    const topping = document.createElement('div');
+    topping.className = 'topping';
+    topping.innerHTML = type; // e.g., '🍓' or '🕯️'
+    topping.style.left = '125px';
+    topping.style.top = '125px';
+    
+    // Make it draggable
+    topping.onmousedown = function(event) {
+        let shiftX = event.clientX - topping.getBoundingClientRect().left;
+        let shiftY = event.clientY - topping.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            topping.style.left = pageX - cake.offsetLeft - shiftX + 'px';
+            topping.style.top = pageY - cake.offsetTop - shiftY + 'px';
+        }
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        topping.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            topping.onmouseup = null;
+        };
+    };
+
+    cake.appendChild(topping);
+}
